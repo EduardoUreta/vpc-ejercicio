5f8b55c8a4fce32600182c931cd5d2ba39fa478e
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-07-11 11:11:09 -0400

    0.0.0.0

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = 3000;
 
 app.get('/', (_, res) => res.send('Aplicación funcionando desde una VPC'));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
